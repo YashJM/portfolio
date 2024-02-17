7695cf19dc05bbe5d7a6c2b743a178ab7f80ef27
YashJM <mistryyash100@gmail.com>
2024-02-17 14:23:35 -0500

    updated yash data source

---
+++ data/yash.js
@@ -0,0 +1,16 @@
+class Yash {
+    constructor() {
+        this.name = 'Yash Mistry';
+        this.occupation = 'Full-stack Developer';
+        this.hobbies = ['coding', 'gaming', 'reading memes', 'Guitar', 'ruibiks cube'];
+        this.personalityTraits = ['ambitious', 'curious', 'caffeine-dependent'];
+    }
+
+    introduce() {
+        console.log(`Hey, I'm ${this.name}! I'm a ${this.occupation} with hobbies like ${this.hobbies.join(', ')}.`);
+        console.log(`People often describe me as ${this.personalityTraits.join(', ')}.`);
+    }
+}
+
+const yash = new Yash();
+yash.introduce();
